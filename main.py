2636ebd4cbbb30ba4f5364a3a86004ea3b2916c4
Kurui02 <joswarramirez02@gmail.com>
2026-02-08 20:19:10 -0400

    logica de imagenes con JSON agregadas

--- main.py
+++ main.py
@@ -15,22 +15,26 @@ color_texto= (45,52,54)
 color_aciertos= (85,239,196)
 white=(255,255,255)
 
-with open("Imagenes_F.JSON","r") as imgF:
-    img_F = json.load(imgF)
-
 class GameState:
     def __init__(self):
         self.state = 'MENU'
         self.difficulty = None
         self.current_stage = 0
         self.total_stages = 10
-        self.stage_pool = {"F":img_F} #lista para generar un pool de imagenes para que no sean las mismas siempre
-        self.found_difs = {"F":img_F} #lista para guardar los clicks correctos 
+        self.stage_pool = [] #lista para generar un pool de imagenes para que no sean las mismas siempre
+        self.found_difs = [] #lista para guardar los clicks correctos 
         self.difs_actuales = [] #lista de (x, y) del nivel actual
         self.circles_draw = []
+
+        with open("Imagenes_F.JSON","r") as imgF:
+            self.all_data = json.load(imgF)["imgs"]
+
         self.font_title = pygame.font.SysFont("Arial", 80, bold=True)
         self.font_btns = pygame.font.SysFont("Arial", 35 , bold=True)
         self.font_ui = pygame.font.SysFont('Arial', 25 , bold=True)
+
+        self.img_izq= None
+        self.img_der= None
     
     def crear_boton(self,texto,y_pos,ancho=250,alto=60):
         x_pos = (screen_width // 2) - (ancho // 2)
@@ -53,15 +57,33 @@ class GameState:
         self.difficulty = diff_level
         self.state = 'PLAYING'
         self.current_stage = 1
+
+        if self.difficulty == 'Facil':
+            self.stage_pool = self.all_data.copy()
+            random.shuffle(self.stage_pool)
+            self.total_stages = len(self.stage_pool)
+            self.cargar_stage_actual()
+
+    def cargar_stage_actual(self):
         self.found_difs = []
         self.circles_draw = []
-        print(f"Iniciando Dificultad : {self.difficulty}")
-        #Aqui se cargaria la pool y se barajaria (random.shuffle)
-        if self.difficulty == 'Facil':
-    
-                       
-        #self.difs_actuales = pool[self.current_stage]['diferencias']
-        
+        self.difs_actuales = []
+
+        id = self.current_stage - 1
+        nivel_data = list(self.stage_pool[id].values())[0]
+
+        path_izq = nivel_data["img_izq"] + ".png" if "." not in nivel_data["img_izq"] else nivel_data["img_izq"]
+        path_der = nivel_data["img_der"] + ".png" if "." not in nivel_data["img_der"] else nivel_data["img_der"]
+
+        self.img_izq = pygame.image.load(path_izq).convert_alpha()
+        self.img_der = pygame.image.load(path_der).convert_alpha()
+
+        self.img_izq = pygame.transform.smoothscale(self.img_izq,(500,500))
+        self.img_der = pygame.transform.smoothscale(self.img_der,(500,500))
+
+        for key in ["punto1", "punto2", "punto3"]:
+            px, py = nivel_data[key]
+            self.difs_actuales.append((650 + px , 150 + py))
 
     def draw_menu(self):
         screen.fill(color_fondo)
@@ -84,18 +106,20 @@ class GameState:
 
         ui_text = self.font_ui.render(f"Dificultad: {self.difficulty} | Stage: {self.current_stage}/{self.total_stages}", True, color_texto)
         screen.blit(ui_text, (20,20))
+
     def draw_stage(self):
         screen.fill(white)
         self.draw_hud()
 
-        #Aqui iria la logica para las imagenes
-        img_a = pygame.image.load()
-        screen.blit(img_a, (50,150))
-        img_b = pygame.image.load()
-        screen.blit(img_b, (650,150))
+        
+        if self.img_izq:
+            screen.blit(self.img_izq, (50, 150))
+            screen.blit(self.img_der, (650, 150))
 
-        for pos in self.circles_draw:
-            pygame.draw.circle(screen, color_aciertos, pos, 35, 5)
+        for (dx, dy) in self.circles_draw:
+            pygame.draw.circle(screen, color_aciertos, (dx, dy), 35, 5)
+
+            pygame.draw.circle(screen, color_aciertos, (dx - 600, dy), 35, 5)
 
         
         pygame.display.flip()
@@ -113,9 +137,7 @@ class GameState:
     def pasar_siguiente_stage(self):
         if self.current_stage < self.total_stages:
             self.current_stage +=1
-            self.found_difs = []
-            self.circles_draw = []
-            #cargar la imagen siguiente
+            self.cargar_stage_actual()
         else:
             self.state = 'Victory'
     
